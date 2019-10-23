328aa4228f3655f11ab832ddc7708108334174b3
k503394469 <k503394469@gmail.com>
2019-10-23 14:47:25 +0900

    笔记本

--- src/main/java/com/liu/servlet/ServletDemo1.java
+++ src/main/java/com/liu/servlet/ServletDemo1.java
@@ -1,8 +1,9 @@
 package com.liu.servlet;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
-
+@WebServlet(name = "servletDemo1",urlPatterns = "/servletDemo1")
 public class ServletDemo1 implements Servlet {
 
     @Override
@@ -17,7 +18,7 @@ public class ServletDemo1 implements Servlet {
 
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("helloworld....");
+        System.out.println("helloworld...");
     }
 
     @Override
--- src/main/java/com/liu/work/Work1.java
+++ src/main/java/com/liu/work/Work1.java
@@ -1,5 +1,6 @@
 package com.liu.work;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.servlet.ServletException;
@@ -9,13 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.text.DateFormat;
+import java.lang.reflect.InvocationTargetException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @WebServlet("/work1")
 public class Work1 extends HttpServlet {
@@ -25,8 +25,16 @@ public class Work1 extends HttpServlet {
         request.setCharacterEncoding("utf-8");
         response.setContentType("text/html;charset=utf-8");
         PrintWriter out = response.getWriter();
-        String method = request.getParameter("method");
+        method = request.getParameter("method");
         if ("ins".equals(method)) {
+            Map<String, String[]> stuMap = request.getParameterMap();
+            try {
+                BeanUtils.populate(Stu.class,stuMap);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
             String name = request.getParameter("name");
             String sex = request.getParameter("sex");
             String age = request.getParameter("age");
@@ -39,11 +47,31 @@ public class Work1 extends HttpServlet {
                 e.printStackTrace();
             }
             String phone = request.getParameter("phone");
-            String hobby = request.getParameter("hobby");
+            String []hobby = request.getParameterValues("hobby");
+            String hobbies="";
+            for (String s:hobby){
+                hobbies+=s+" ";
+            }
             String sql="insert into stu values (?,?,?,?,?,?)";
-            int update = jt.update(sql, name, sex, Integer.parseInt(age),dateDf, phone, hobby);
-        }else if ("view".equals(method)){
+            int update = jt.update(sql, name, sex, Integer.parseInt(age),dateDf, phone, hobbies);
 
+        }else if ("view".equals(method)){
+            String sql="select * from stu";
+            List<Stu> stus=jt.query(sql,(resultSet ,i )->{
+                Stu temp=new Stu();
+                temp.setName(resultSet.getString(1));
+                temp.setSex(resultSet.getString(2));
+                temp.setAge(resultSet.getInt(3));
+                temp.setDate(resultSet.getDate(4));
+                temp.setPhone(resultSet.getString(5));
+                temp.setHobby(resultSet.getString(6));
+                return temp;
+            });
+            out.print("<table border='1'>");
+            for (Stu temp:stus){
+                out.print("<tr><td>"+temp.getName()+"</td><td>"+temp.getSex()+"</td><td>"+temp.getAge()+"</td><td>"+temp.getDate()+"</td><td>"+temp.getPhone()+"</td><td>"+temp.getHobby()+"</td></tr>");
+            }
+            out.print("</table>");
         }
     }
 
@@ -55,7 +83,6 @@ public class Work1 extends HttpServlet {
     public void init() throws ServletException {
         try {
             jt = new JdbcTemplate(JDBCUtils.getDataSource());
-
         } catch (Exception e) {
             e.printStackTrace();
         }
